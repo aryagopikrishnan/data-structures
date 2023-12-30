d7e87d2a35b75039a81911904aa79f7cb11c9ecb
Arya <aryagxr@gmail.com>
2023-12-30 17:50:50 +0530

    bubble sort

---
+++ 01-sort/bubblesort.py
@@ -0,0 +1,14 @@
+# bubble sort
+
+def BubbleSort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+    return my_list
+
+# prints in incremental order
+print(BubbleSort([5,3,1,2,6,4]))
+
